02a78e36199418e45d3e8dc0355589331304a248
Saiqiong XIAO <saxiao@e3r2p21.42.fr>
2018-06-27 21:16:35 +0200

    update

--- helper.c
+++ helper.c
@@ -6,7 +6,7 @@
 /*   By: saxiao <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/27 14:39:10 by saxiao            #+#    #+#             */
-/*   Updated: 2018/06/27 14:39:14 by saxiao           ###   ########.fr       */
+/*   Updated: 2018/06/27 19:31:15 by saxiao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,3 +23,19 @@ t_table			*malloc_add_sh(void)
 	add->next = NULL;
 	return (add);
 }
+/*
+int			replace_home(char *word, char **env)
+{
+	char	temp[MAX_BUF];
+	char	*vari_value;
+
+	ft_bzero(temp, MAX_BUF);
+	if (*word && *word == '~')
+	{
+		vari_value = ft_getenv(env, "HOME");
+		if (!vari_value)
+			return(return_message("Undefined HOME variable.\n", 1, 2));
+		ft_strcpy(tem
+	}
+}
+*/
